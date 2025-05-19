5a443ea68f9e275401b4f74a7b4345946055a4c7
Ken-2511 <iwmain@outlook.com>
2025-05-19 12:31:34 -0400

    The seagull taught pigeon pygame

--- Paint/Paint.py
+++ Paint/Paint.py
@@ -6,12 +6,18 @@ from pygame.locals import *
 #initalize the game
 pygame.init()
 
+# strokes = [stroke1, stroke2, stroke3]
+# stroke1 = [(0, 0), (100, 100), (200, 200), ...]
+strokes = []
+
 # set background parameters
 size = width, height = 800, 600
-bg = (0, 0 ,0)
+bg_color = (0, 0 ,0)
 screen = pygame.display.set_mode(size)
-screen.fill(bg)
-drawing_screen = screen.copy()
+screen.fill(bg_color)
+
+# clock
+clock = pygame.time.Clock()
 
 drawing = 0
 
@@ -19,7 +25,6 @@ mouse_pos = pygame.mouse.get_pos()
 last_mouse_pos = mouse_pos
 
 while True:
-    screen.blit(drawing_screen, (0, 0))
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
@@ -28,25 +33,36 @@ while True:
             if event.key == K_ESCAPE:
                 pygame.quit()
                 sys.exit()
+            if event.key == K_RETURN:
+                # print the stroke
+                for idx, stroke in enumerate(strokes, 1):
+                    print(f"Stroke {idx}: {stroke}")
+            if event.key == K_p:
+                # redraw the stroke with a different color
+                pass
 
         if event.type == MOUSEBUTTONDOWN:
             if event.button == 1:
                 drawing = 1
-                last_mouse_pos = pygame.mouse.get_pos()
+                last_mouse_pos = event.pos
+                # create a new stroke
+                strokes.append([])
+                strokes[-1].append(event.pos)
 
         if event.type == MOUSEBUTTONUP:
             if event.button == 1:
                 drawing = 0
-                last_mouse_pos = pygame.mouse.get_pos()
 
-    # draw the cursor
-
-    if drawing == 1:
-        mouse_pos = pygame.mouse.get_pos()
-        pygame.draw.line(drawing_screen, (255, 255, 255), last_mouse_pos, mouse_pos,5)
-        # update mouse position
-        last_mouse_pos = mouse_pos
+        if event.type == MOUSEMOTION and drawing == 1:
+            mouse_pos = event.pos
+            pygame.draw.line(screen, (255, 255, 255), last_mouse_pos, mouse_pos,5)
+            # update mouse position
+            last_mouse_pos = mouse_pos
+            # update stroke
+            strokes[-1].append(mouse_pos)
 
     # double buffer flip
     pygame.display.flip()
-    time.sleep(1/60)
+
+    # 60 FPS
+    clock.tick(60)
