60e95534dff110bdda80091fbc6c63e3faaad96b
Sarah Jia <he.jia@mail.utoronto.ca>
2025-06-12 23:42:42 -0400

    Day6 homework

--- Day6/Day6.py
+++ Day6/Day6.py
@@ -3,10 +3,36 @@
 import base64
 
 def encrypt(data, key):
-    pass
+    # data(string) -> data(bytes)
+    data_bytes = data.encode("utf-8")
+    # key(string) -> key(byte)
+    key_bytes = key.encode("utf-8")
+    # xor
+    data_encrypted_list = []
+    for i in range(len(data_bytes)):
+        data_encrypted_list.append(data_bytes[i] ^ key_bytes[i % len(key_bytes)])
+
+    data_encrypted_bytes = bytes(data_encrypted_list)
+    #print(data_encrypted_bytes)
+    data_encrypted_base64 = base64.b64encode(data_encrypted_bytes)
+    data_encrypted = data_encrypted_base64.decode("utf-8")
+    return data_encrypted
+
 
 def decrypt(data, key):
-    pass
+    # data(string)（是一个base64翻译成的string） -> data(bytes)
+    data_bytes = base64.b64decode(data)
+    # key(string) -> key(byte)
+    key_bytes = key.encode("utf-8")
+    # xor
+    data_decrypted_list = []
+    for i in range(len(data_bytes)):
+        data_decrypted_list.append(data_bytes[i] ^ key_bytes[i % len(key_bytes)])
+
+    data_decrypted_bytes = bytes(data_decrypted_list)
+    # print(data_decrypted_bytes)
+    data_decrypted = data_decrypted_bytes.decode("utf-8")
+    return data_decrypted
 
 if __name__ == '__main__':
     choice = input("请选择操作：1.加密 2.解密\n")
